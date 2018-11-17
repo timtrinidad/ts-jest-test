362518d57e3f4e9434f111d00441eb91908ad5c3
Tim Trinidad <timtrinidad@gmail.com>
2018-11-17 12:31:37 -0500

    initial commit

---
+++ packages/one/src/TestComponent.spec.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import TestComponent from 'src/TestComponent';
+import { shallow } from 'enzyme';
+
+it('does something', () => {
+  const wrapper = shallow(<TestComponent>child</TestComponent>);
+  expect(wrapper).not.toBeNull();
+})
